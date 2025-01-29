b1188fd5dc09953809c729657ad9bfc4b14704e0
TenchiniM <maycontss@gmail.com>
2025-01-29 19:48:00 -0300

    adicionando biblioteca ts-node e typescript

---
+++ dist/teste.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome = "Maycon";
+let idade = 90;
+console.log(`Oi, meu nome é: ${nome} e eu tenho ${idade}`);
---
+++ teste.ts
@@ -0,0 +1,4 @@
+let nome: string = "Maycon";
+let idade: number = 90;
+
+console.log(`Oi, meu nome é: ${nome} e eu tenho ${idade}`);

60c86f3bff64e6fefb7517758cde508fba3524f1
Maycon Tenchini <68251927+TenchiniM@users.noreply.github.com>
2025-01-30 15:45:05 -0300

    Alterando para o cabeçalho passar em formato json.
    permite que o servidor entenda requisições em formato url
    
    passando o diretório da pasta public

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,37 @@
-import  express  from "express";
+import express from "express"; // importando biblioteca express
+import helmet from 'helmet'; // importando biblioteca helmet
+import path from "path"; // importando biblioteca path 
+//path trabalha com pastas e arquivos
 
-const server = express();
+const server = express(); // criando servidor em express
+
+server.use(helmet());
+// use é função do express que permite colocar as coisas que queremos usar no servidor
+// colocando o helmet dentro do servidor
+// helmet é um pacote que ajuda a proteger o servidor de algumas vulnerabilidades conhecidas
+server.use(express.json());
+// diz que o cabeçalho da requisição é um json 
+// express.json() é uma função que permite que o servidor entenda requisições em formato json
+server.use(express.urlencoded({ extended: true }));
+// armazena informações que pode ser passadas para o servidor
+// permite que o servidor entenda requisições em formato urlencoded (url)
+server.use(express.static(path.join(__dirname, "../public")));
+// express.static é uma função que permite que o servidor acesse arquivos estáticos
+// path.join é uma função que junta caminhos de arquivos
+// __dirname é uma variável global que retorna o diretório do arquivo atual
+// "../public" é o caminho do diretório que queremos acessar
 
 server.get("/", (req, res) => {
-  res.send("Hello World!"); //res - item de resposta
-});                           //.send - envia a resposta
+    let msg = "Hello World!";
+
+    res.json({ msg });
+    //res - item de resposta
+    //.send - envia a resposta
+    // .json - envia a resposta em formato json
+});
+
 
 server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000");
 });
+

7a2a3e4a776678c869aaa01cbe45086b9cb46727
Maycon Tenchini <68251927+TenchiniM@users.noreply.github.com>
2025-01-30 11:51:56 -0300

    alterações na mensagem do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { createServer } from `node:http`;
 
 const server = createServer((req, res) => {
-    res.end('Hello World!'); // finalizando exibindo um texto
+    let nome: string = "Maycon";
+    let idade: number = 23; 
+    res.end(`Ola, meu nome e: ${nome} e tenho: ${idade} anos `); // finalizando exibindo um texto
 }); // variavel server recebe a função createServer que recebe uma função com dois parametros req e res
 server.listen(3000, () => {
     console.log('Server is running on http://localhost:3000'); // exibe uma mensagem no console

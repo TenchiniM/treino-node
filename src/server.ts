b1801bf010210ace37203f056cc0161e489ceed4
Maycon Tenchini <68251927+TenchiniM@users.noreply.github.com>
2025-01-31 16:18:59 -0300

    Configurando nova rota dinâmica

--- src/server.ts
+++ src/server.ts
@@ -29,16 +29,45 @@ server.get('/ping', (req, res) => {
 });
 
 server.post('/produtos', (req, res) => {
-    res.json({pong: true});
+    res.json({ pong: true });
 }); // diretorio para post
 
 server.get('/produtos', (req, res) => {
-    res.json({pong: true});
+    let produtos = [];
+    res.json({ produtos: [] });
 }); // /produtos, outro diretorio
+
+server.get('/produtos/:id', (req, res) => {
+    const { id } = req.params;
+    // id é uma variável que recebe o valor do parametro da requisição
+    res.json({ id, name: 'Teclado XYZ', price: 90 });
+    // responde com um json com as chaves id, name e price
+    console.log(req.params);
+
+    res.json({ name: 'Produto 1', price: 100 });
+}); // /diretorio com id, dinamico
+// qualquer coisa digitada depois de /produtos/ é considerado um id
+
+server.get('/voos/:from/:to', (req, res) => {
+    const { from, to } = req.params;
+    // from e to são variáveis que recebem os valores dos parametros da requisição
+    // req.params é um objeto que contém os parametros da requisição
+    // tudo digitado depois da primeira barra, aparece no console
+    res.json({
+        flight: {
+            from: from.toLocaleUpperCase(),
+            to: to.toUpperCase(),
+            price: 123456,
+        } 
+    });
+    console.log(req.params); // o que aparece na pagina
+});
 
 server.get("/", (req, res) => {
     // "/" é a rota principal do servidor
     let msg = "Hello World!";
+
+
 
     res.json({ msg });
     //res - item de resposta
